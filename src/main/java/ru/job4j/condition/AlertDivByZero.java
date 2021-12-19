9a8bb1958102ea62bbd4dbb63d4263cba2f368fe
nzekynov <nzekynov@inbox.ru>
2021-12-19 11:27:57 +0300

    4.1. Операторы ветвлений [#3611 #241705]

--- src/main/java/ru/job4j/condition/AlertDivByZero.java
+++ src/main/java/ru/job4j/condition/AlertDivByZero.java
@@ -4,6 +4,7 @@ public class AlertDivByZero {
     public static void main(String[] args) {
         possibleDiv(4);
         possibleDiv(0);
+        possibleDiv(-1);
     }
 
     public static void possibleDiv(int number) {
